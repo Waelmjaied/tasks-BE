1280396bc0d0d2bf4aca970e38dce522dcd727a5
Waelmjaied <96178658+Waelmjaied@users.noreply.github.com>
2024-11-20 16:19:18 +0100

    app.js

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 const express = require('express');
 const tasksRoute = require("./routes/admin")
 const authRoute = require("./routes/auth")
